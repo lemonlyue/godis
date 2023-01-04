0da96b516724421c432e3f3ce17f00aca4be1f94
lemon_lyue <lemon_lyue@163.com>
2023-01-04 22:45:53 +0800

    feat: redis通信协议(RESP-redis序列化协议)

--- redis/parser/parser.go
+++ redis/parser/parser.go
@@ -1,1 +1,36 @@
 package parser
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+)
+
+func parse0(rawReader io.Reader) {
+	reader := bufio.NewReader(rawReader)
+	for {
+		// 获取文本协议
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+
+		}
+
+		length := len(line)
+		if length <= 2 || line[length-2] != '\r' {
+			// empty line
+			continue
+		}
+
+		line = bytes.TrimSuffix(line, []byte{'\r', '\n'})
+		// 字符开头
+		switch line[0] {
+		case '+': // 单行字符串
+		case '-': // 错误提示
+		case ':': // 整数值
+		case '$': // 多行字符串
+		case '*': // 数组
+		default:
+
+		}
+	}
+}
